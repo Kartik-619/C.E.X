c80f5a96b72ccd161c605e9cdac565841ae0c69b
Kartik Sharma <poojan4604@gmail.com>
2026-08-11 16:20:40 +0530

    Update wallet.ts
    implementing wallet service

--- backend/src/engine/services/wallet/wallet.ts
+++ backend/src/engine/services/wallet/wallet.ts
@@ -1,3 +1,25 @@
-export class Wallet{
-    
+import type { Inmemory_WalletStore } from "../../../store/wallet-store";
+import type { Balance } from "../../interface/Ibalance";
+import type { IWallet } from "../../interface/Iwallet";
+
+export class Wallet implements  IWallet<Balance>{
+    private store:Inmemory_WalletStore;
+    constructor(store:Inmemory_WalletStore){
+        this.store=store;
+    };
+    async getBalance(userId: string, asset: string): Promise<Balance> {
+      return  this.store.getBalance(userId,asset)
+    }
+    async lockFunds(userId: string, asset: string, amount: number): Promise<void> {
+        this.store.lockFunds(userId,asset,amount);
+    }
+    async unlockFunds(userId: string, asset: string, amount: number): Promise<void> {
+        this.store.unlockFunds(userId,asset,amount);
+    }
+    async deposit(userId: string, asset: string, amount: number): Promise<void> {
+        this.store.deposit(userId,asset,amount)
+    }
+    async settleTrade(trade: Balance): Promise<void> {
+        this.store.settleTrade(trade);
+    }
 }
